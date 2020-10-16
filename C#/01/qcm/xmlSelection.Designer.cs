5cac7c99a37ba08b76e7509d86eabe5209d3a976
AntoniusKhalius <khalil.elbali@gmail.com>
2020-10-16 16:53:10 +0200

    Modification xmlSelection v1

--- C#/01/qcm/xmlSelection.Designer.cs
+++ C#/01/qcm/xmlSelection.Designer.cs
@@ -32,16 +32,16 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // xmlListBox
             // 
-            this.xmlListBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.xmlListBox.FormattingEnabled = true;
-            this.xmlListBox.Location = new System.Drawing.Point(0, 0);
+            this.xmlListBox.Location = new System.Drawing.Point(0, 39);
             this.xmlListBox.Name = "xmlListBox";
-            this.xmlListBox.Size = new System.Drawing.Size(284, 186);
+            this.xmlListBox.Size = new System.Drawing.Size(284, 147);
             this.xmlListBox.TabIndex = 0;
             // 
             // panel1
@@ -75,17 +75,29 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selection d'un fichier XML";
+            // 
             // xmlSelection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.xmlListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "xmlSelection";
             this.Text = "xmlSelection";
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -95,5 +107,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- C#/01/qcm/xmlSelection.cs
+++ C#/01/qcm/xmlSelection.cs
@@ -12,25 +12,26 @@ namespace qcm
     public partial class xmlSelection : Form
     {
         private Form sender;
+        private string path;
 
         public xmlSelection(object sender)
         {
             this.sender = (Form)sender;
 
-            string path = "..\\..\\xml";
+            this.path = "..\\..\\xml\\";
             InitializeComponent();
             foreach (string sFileName in System.IO.Directory.GetFiles(path))
             {
                 if (System.IO.Path.GetExtension(sFileName) == ".xml")
                 {
-                    this.xmlListBox.Items.Add(sFileName);
+                    this.xmlListBox.Items.Add(System.IO.Path.GetFileName(sFileName));
                 }
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            questionnaire questionnaire = new questionnaire(this.xmlListBox.SelectedItem.ToString());
+            questionnaire questionnaire = new questionnaire(this.path + this.xmlListBox.SelectedItem.ToString());
             questionnaire.MdiParent = this.sender;
             questionnaire.Show();
             this.Close();
